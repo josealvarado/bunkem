6d16512ba2a5c0943a067dae96b278f8411a115f
Jose Alvarado <josealvarado@joses-mbp.gateway.sonic.net>
2017-01-28 10:44:27 -0800

    Updates

--- bunkem/bunkem/Controllers/Filter/BKMAgePreferenceViewController.swift
+++ bunkem/bunkem/Controllers/Filter/BKMAgePreferenceViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class BKMAgePreferenceViewController: UIViewController {
+class BKMAgePreferenceViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    var ref: FIRDatabaseReference?
+    
+    var lowerBoundAge = [18, 19, 20, 21]
+    var topBoundAge = [32, 33, 34, 35]
+    
+    var low = 18
+    var top = 35
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        ref = FIRDatabase.database().reference()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +33,30 @@ class BKMAgePreferenceViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        ref?.child("users").child(CurrentUser.user.user.uid).observeSingleEvent(of: .value, with: { snapshot in
+            if let userInfo = snapshot.value as? Dictionary<String, AnyObject> {
+                
+                if let low = userInfo["lowerBoundAge"] as? Int {
+                    
+                    self.low = low
+                    
+                    if let index = self.lowerBoundAge.index(of: low) {
+                        self.pickerView.selectRow(index, inComponent: 0, animated: true)
+                    }
+                }
+                
+                if let top = userInfo["topBoundAge"] as? Int {
+                    self.top = top
+                    
+                    if let index = self.topBoundAge.index(of: top) {
+                        self.pickerView.selectRow(index, inComponent: 1, animated: true)
+                    }
+                }
+            }
+        })
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -31,5 +66,55 @@ class BKMAgePreferenceViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if component == 0 {
+            return lowerBoundAge.count
+        }
+        
+        return topBoundAge.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if component == 0 {
+            return "\(lowerBoundAge[row])"
+        }
+
+        
+        return "\(topBoundAge[row])"
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if component == 0 && row < lowerBoundAge.count {
+            self.low = self.lowerBoundAge[row]
+        }
+        
+        if row < self.topBoundAge.count {
+            self.top = self.topBoundAge[row]
+        }
+        
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 100
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 36.0
+    }
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        
+        let userInfo = ["lowerBoundAge": low, "topBoundAge": top]
+        ref?.child("users").child(CurrentUser.user.user.uid).updateChildValues(userInfo as [NSObject : AnyObject])
+
+    }
 
 }
